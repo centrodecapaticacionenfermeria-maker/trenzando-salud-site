2ba03c264e1eafcf52f24d1658910cb8f4fa4eec
Centro de Capacitación Enfermería <centrodecapacitacionenfermeria@gmail.com>
2025-11-08 16:39:23 -0500

    cambios cursos

---
+++ public/css/main.js
@@ -0,0 +1,13 @@
+// Mostrar/ocultar más información al hacer clic
+document.querySelectorAll('.show-more').forEach(button => {
+  button.addEventListener('click', function () {
+    const moreInfo = this.nextElementSibling; // El div con clase 'more-info'
+    if (moreInfo.style.display === 'none' || !moreInfo.style.display) {
+      moreInfo.style.display = 'block';
+      this.textContent = 'Ver menos';
+    } else {
+      moreInfo.style.display = 'none';
+      this.textContent = 'Más información';
+    }
+  });
+});
